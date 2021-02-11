a26cf914c1c9e52c43565f9936b408cefbcec9e3
Melvyn Tutesigensi <melvyntut@gmail.com>
2021-02-11 12:58:54 +0000

    first ui commit

--- app/src/main/java/com/example/canteenapp/Login.java
+++ app/src/main/java/com/example/canteenapp/Login.java
@@ -2,13 +2,20 @@ package com.example.canteenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loign);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void confirmButton(View view){
+        Intent intent = new Intent(this, MainMenuAfterLogin.class);
+        startActivity(intent);
     }
 }
