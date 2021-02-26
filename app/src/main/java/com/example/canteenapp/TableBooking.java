867924072bb1482f7969fed37500cd1ba6c8accc
christopher wright <chriswright21@live.co.uk>
2021-02-26 15:08:44 +0000

    added availability to buttons

--- app/src/main/java/com/example/canteenapp/TableBooking.java
+++ app/src/main/java/com/example/canteenapp/TableBooking.java
@@ -1,12 +1,16 @@
 package com.example.canteenapp;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.icu.text.UFormat;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,78 +29,112 @@ public class TableBooking extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     CollectionReference eatIn_db = db.collection("eatIn_timeslots");
     private LinearLayout bookingsLayout;
+
     private ArrayList<String> availableTimes = new ArrayList<>();
+    private ArrayList<Button> buttons = new ArrayList<>();
+
+    private Button tenButton;
+    private Button tenThirtyButton;
+    private Button elevenButton;
+    private Button elevenThirtyButton;
+    private Button twelveButton;
+    private Button twelveThirtyButton;
+    private Button oneButton;
+    private Button oneThirtyButton;
+    private Button twoButton;
+    private Button twoThirtyButton;
+    private Button threeButton;
+    private Button threeThirtyButton;
+    private Button fourButton;
 
     final String TAG = "Table_Booking";
-    ///still working on this
+
     @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_booking);
+
+        tenButton = findViewById(R.id.ten_button);
+        tenThirtyButton = findViewById(R.id.ten_thirty_button);
+        elevenButton = findViewById(R.id.eleven_button);
+        elevenThirtyButton = findViewById(R.id.eleven_thity_button);
+        twelveButton = findViewById(R.id.twelve_button);
+        twelveThirtyButton = findViewById(R.id.twelve_thirty_button);
+        oneButton = findViewById(R.id.one_button);
+        oneThirtyButton = findViewById(R.id.one_thirty_button);
+        twoButton =findViewById(R.id.two_button);
+        twoThirtyButton = findViewById(R.id.two_thity_button);
+        threeButton = findViewById(R.id.three_button);
+        threeThirtyButton = findViewById(R.id.three_thirty_button);
+        fourButton = findViewById(R.id.four_button);
+
+        buttons.add(tenButton);
+        buttons.add(tenThirtyButton);
+        buttons.add(elevenButton);
+        buttons.add(elevenThirtyButton);
+        buttons.add(twelveButton);
+        buttons.add(twelveThirtyButton);
+        buttons.add(oneButton);
+        buttons.add(oneThirtyButton);
+        buttons.add(twoButton);
+        buttons.add(twoThirtyButton);
+        buttons.add(threeButton);
+        buttons.add(threeThirtyButton);
+        buttons.add(fourButton);
 
         bookingsLayout = findViewById(R.id.booking_linear_layout);
 
         ///get current time/date
         LocalTime timeNow = LocalTime.now();
+        //get available slots and highlight buttons
+        getAvailableTimes(timeNow);
+    }
 
+
+    private void getAvailableTimes(LocalTime timeNow){
 
         eatIn_db.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
                 for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
-
                     LocalTime timeSlotFromDb = LocalTime.parse(snapshots.getId());
                     // get slots after time now
                     if (timeNow.isBefore(timeSlotFromDb)) {
-
                         CollectionReference timeslot_collection = db.collection("eatIn_timeslots").document(snapshots.getId()).collection("bookings");
-
                         timeslot_collection.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                             @Override
                             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
                                 int count = 0;
                                 boolean spaceAvailable = true;
-
                                 for (QueryDocumentSnapshot bookingSnapshot : queryDocumentSnapshots) {
                                     if (bookingSnapshot.getData().get("student_num") != null) {
-
                                         count++;
                                     }
                                     if (count >= 3) {
                                         spaceAvailable = false;
                                     }
-
                                 }
-                                if (spaceAvailable) {
-                                    ///create a new textView and add to list view
-                                    //Log.d(TAG, "adding" + timeSlotFromDb);
-                                    availableTimes.add(timeSlotFromDb.toString());
-                                    TextView newTextView = new TextView(getApplicationContext());
-                                    newTextView.setText(timeSlotFromDb.toString());
-                                    newTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                                    newTextView.setTextSize(26);
-                                    bookingsLayout.addView(newTextView);
-
-
+                                if (spaceAvailable){
+                                    for (Button button : buttons){
+                                        if (timeSlotFromDb.toString().equals(button.getText().toString())){
+                                            button.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+                                                    Toast.makeText(TableBooking.this, "cklicked", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                        }
+                                        else{
+                                            button.setBackgroundColor(Color.rgb(201,201,201));
+                                        }
+                                    }
                                 }
                             }
                         });
-
                     }
                 }
-
             }
-
-
         });
-
-
-        //check which slots are free in next two hours
-        ///////iterate through slots and check number of students
-        ///offer slots for next two hours that are free
-        //update db
     }
 }
