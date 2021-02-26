e85593e872f046184d6aab0cfe241d59f620cbea
christopher wright <chriswright21@live.co.uk>
2021-02-26 11:07:16 +0000

    started doing book table methods and building db

--- app/src/main/java/com/example/canteenapp/TableBooking.java
+++ app/src/main/java/com/example/canteenapp/TableBooking.java
@@ -1,7 +1,9 @@
 package com.example.canteenapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -13,30 +15,48 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 public class TableBooking extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference eatIn_db = db.collection("table_timeslots");
+    CollectionReference eatIn_db = db.collection("eatIn_timeslots");
 
     final String TAG = "Table_Booking";
     ///still working on this
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_booking);
-        ///get current time/date
-        Date currentTime = Calendar.getInstance().getTime();
-        Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date = formatter.format(currentTime);
 
+        ///get current time/date
+        LocalTime timeNow = LocalTime.now();
         eatIn_db.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots){
-                    Log.d(TAG, "onSuccess: " + snapshots.getData());
+                    LocalTime timeSlotFromDb = LocalTime.parse(snapshots.getId());
+                    // get slots after time now
+                    if (timeNow.isBefore(timeSlotFromDb)){
+                        Log.d(TAG, " time " + timeSlotFromDb);
+                        CollectionReference timeslot_collection = db.collection("eatIn_timeslots").document(snapshots.getId()).collection("bookings");
+                        timeslot_collection.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for (QueryDocumentSnapshot bookingSnapshot : queryDocumentSnapshots){
+                                    Log.d(TAG, "booking" + bookingSnapshot.getData().get("student_num"));
+                                    Log.d(TAG, "booking" + bookingSnapshot.getData().get("date"));
+
+                                }
+                            }
+                        });
+                        // check if space
+
+                    }
                 }
 
             }
