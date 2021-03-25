277b75c175a4f6e3150c31dbc1dd26f67b0d9ccb
christopher wright <chriswright21@live.co.uk>
2021-03-25 12:33:53 +0000

    test added

--- app/src/androidTest/java/com/example/canteenapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/canteenapp/ExampleInstrumentedTest.java
@@ -2,12 +2,19 @@ package com.example.canteenapp;
 
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static java.util.regex.Pattern.matches;
 import static org.junit.Assert.*;
 
 /**
